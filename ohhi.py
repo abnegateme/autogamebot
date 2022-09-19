cfb2389bbc347058e70da5e410eef547c290ddb2
Alexander <abnegateme@gmail.com>
2022-09-19 08:19:32 +0300

    rewrite initial step with find_grid method - now script without 'n' arg - need to optimize: mouse clicks

--- ohhi.py
+++ ohhi.py
@@ -13,7 +13,7 @@ import sys
 
 from image_processing import find_grid_by_maxima
 
-def main(n, adb = False, mss = False, window_name='scrcpy'):
+def main(adb = False, mss = False, window_name='scrcpy'):
 
     if adb and mss:
         print('choose one method')
@@ -43,39 +43,34 @@ def main(n, adb = False, mss = False, window_name='scrcpy'):
         image = window.get_roi_image()
         mouse = Controller()
 
-    grid_x, grid_y = find_grid_by_maxima(image, refine=True)
+    grid_x, grid_y = find_grid_by_maxima(image, show=True)
 
+    N_current = np.floor((len(grid_x) + len(grid_y)) / 2)
 
+    x_half_width = np.mean(np.diff(grid_x)) / 2
+    y_half_width = np.mean(np.diff(grid_y)) / 2
 
-    sys.exit()
+    xx, yy = np.meshgrid(grid_x + x_half_width, grid_y + y_half_width)
 
+    # for x in grid_x + x_half_width:
+    #     for y in grid_y + y_half_width:
+    #         cv2.drawMarker(image, (int(x), int(y)), (255,0,255))
 
-
-    # cv2.imshow('res', res)
+    # cv2.imshow('grid', image)
     # cv2.waitKey()
-
-    lines = cv2.HoughLinesP(res, 1, math.pi / 180, 2, None, 200, 5)
-    lines = lines.reshape(lines.shape[0], lines.shape[2])
-    mask = lines[:, 0] - lines[:, 2] == 0
-    lines = lines[mask]
-    # print(mask)
     # sys.exit()
-    for line in lines:
-        cv2.line( image_s, (line[0], line[1]), (line[2], line[3]), (255,0,255), 1, cv2.LINE_AA);
 
-    # print(lines)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)[:, :, 0]
 
-    cv2.imshow('image', image_s)
-    cv2.waitKey()
+    # rng = lambda x: np.arange(x, 2 * N_current * x, 2 * x)
+    # xx, yy = np.meshgrid(rng(math.floor(0.5 * image.shape[1] / N_current)),
+    #                      rng(math.floor(0.5 * image.shape[0] / N_current)))
 
-    sys.exit()
+    none_value = 60
+    false_values = [25, 24]
+    true_values = [114, 113]
 
-
-    rng = lambda x: np.arange(x, 2 * n * x, 2 * x)
-    xx, yy = np.meshgrid(rng(math.floor(0.5 * width / n)),
-                         rng(math.floor(0.5 * height / n)))
-
-    init_plate = image[yy, xx].astype(np.int)
+    init_plate = image[yy.astype(int), xx.astype(int)].astype(np.int)
     print('init_plate\n', init_plate)
     plate = init_plate.copy()
     plate[plate == none_value] = 0
@@ -90,7 +85,7 @@ def main(n, adb = False, mss = False, window_name='scrcpy'):
 
     def compute_1st(line):
         is_compute = False
-        for p in np.arange(1, n-1):
+        for p in np.arange(1, N_current-1):
             slc = line[p-1:p+2]
             idx = (states == slc).all(axis=1).nonzero()[0]
             if idx.size > 0:
@@ -107,7 +102,7 @@ def main(n, adb = False, mss = False, window_name='scrcpy'):
 
     def compute_2nd(plate):
         is_compute = False
-        for idx in range(0, n):
+        for idx in range(0, N_current):
             if np.sum(plate[idx, :] == 0) == 2:
                 is_compute = True
                 ids = np.argwhere(plate[idx, :] == 0).flatten()
@@ -130,7 +125,7 @@ def main(n, adb = False, mss = False, window_name='scrcpy'):
         print(f'iter: {itr}')
         print(plate)
         is_compute = False
-        for idx in range(0, n):
+        for idx in range(0, N_current):
             plate[:, idx], is_compute_row = compute_1st(plate[:, idx])
             is_compute = is_compute or is_compute_row
             plate[idx, :], is_compute_column = compute_1st(plate[idx, :])
@@ -158,16 +153,19 @@ def main(n, adb = False, mss = False, window_name='scrcpy'):
         for state, x, y in zip(plate.ravel(), xx.ravel(), yy.ravel()):
             if x == xx.ravel()[0]:
                 shift_x = 3
-            elif x == xx.ravel()[n-1]:
+            elif x == xx.ravel()[N_current-1]:
                 shift_x = -3
             mouse.position = window.roi[0] + x + shift_x, window.roi[1] + y
-            time.sleep(0.1)
+            time.sleep(0.02)
             if state == 1:
                 mouse.click(Button.left, count=1)
             elif state == -1:
-                mouse.click(Button.left, count=2)
+                mouse.click(Button.left, count=1)
+                time.sleep(0.02)
+                mouse.click(Button.left, count=1)
             else:
                 continue
+            time.sleep(0.02)
 
     if adb:
         for state, x, y in zip(plate.ravel(), xx.ravel(), yy.ravel()):
@@ -184,7 +182,6 @@ def main(n, adb = False, mss = False, window_name='scrcpy'):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('n', type=int)
     parser.add_argument('--adb', action='store_true')
     parser.add_argument('--mss', action='store_true')
     parser.add_argument('--window_name', default='scrcpy', type=str)
